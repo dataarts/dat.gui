8126a0cf4b46c193023b5c088198d0ee164c1a2b
George Michael Brower <georgebrower@google.com>
2011-01-23 14:39:07 -0700

    Initial files

--- gui.js
+++ gui.js
@@ -1,7 +1,30 @@
 var GUI = new function() {
 
-	this.add = function(a) {
-		console.log(typeof a);
+
+	
+	var addHandlers = {
+		
+		"number": function() {
+			//
+		}
+		
 	}
 	
-}
+
+	this.add = function() {
+	
+		var object = arguments[0];
+		var property = arguments[1];
+	
+		var value = object[property];
+		var type = typeof value;
+		
+		if (addHandlers[type]) {
+			
+		} else { 
+			// don't know how to handle this data type
+		}
+		
+	}
+	
+};
