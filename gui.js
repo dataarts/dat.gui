b3127f12ed197722ba1ba13436b179c6bfd6dc26
George Michael Brower <georgebrower@google.com>
2011-01-23 15:35:19 -0700

    Updated README and fleshed out gui.js

--- gui.js
+++ gui.js
@@ -1,18 +1,58 @@
 var GUI = new function() {
 
-
+	
+	// Contains list of properties we've add to the GUI in the following format:
+	// [object, propertyName, controllerDomElement]
+	var registeredProperties = [];
+	
+	var domElement;
+	var started = false;
+	this.start = function() {
+		
+		domElement = document.createElement('div');
+		domElement.setAttribute('id', 'guidat');
+		
+		
+		started = true;
+	}
+	
 	this.add = function() {
 	
-		var object = arguments[0];
-		var property = arguments[1];
+		if (!started) {
+			error("Make sure to call GUI.start() in the window.onload function");
+			return;
+		}
 	
-		var value = object[property];
+		var object = arguments[0];
+		var propertyName = arguments[1];
+	
+		var value = object[propertyName];
+		
+		// Does this value exist? Is it accessible?
+		if (value == undefined) {
+			error(object + " either has no property \""+propertyName+"\", or the property is inaccessible.");
+			return;
+		}
+		
 		var type = typeof value;
 		var handler = addHandlers[type];
-		if (handler) {
-		} else { 
-			console.error("I don't know how to handle data type: " + type);
+		
+		// Do we know how to deal with this data type?
+		if (handler == undefined) {
+			error("Cannot create controller for data type \"" + object + "\"");
+			return;
 		}
+	
+		var controllerDomElement = handler.apply(this, arguments);
+		
+		// Were we able to make the controller?
+		if (!controllerDomElement) {		
+			error("Error creating controller for \""+propertyName+"\".");	
+			return;
+		}
+		
+		// Success.
+		registeredProperties.push([object, propertyName, controllerDomElement]);			
 		
 	}
 	
@@ -35,5 +75,9 @@ var GUI = new function() {
 		},
 		
 	};
+	
+	var error = function(str) {
+		console.error("[GUI ERROR] " + str);
+	}
 	
 };
