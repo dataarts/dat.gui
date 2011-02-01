0aa2e7eea518547ef043c8664300e9cfd1fd3609
George Michael Brower <georgebrower@google.com>
2011-02-01 13:45:22 -0500

    Moved Controller classes to GUI namespace

--- controllers/controller.function.js
+++ controllers/controller.function.js
@@ -1,12 +1,11 @@
-var FunctionController = function() {
+GUI.FunctionController = function() {
 	this.type = "function";
 	var that = this;
-	Controller.apply(this, arguments);
+	GUI.Controller.apply(this, arguments);
 	this.domElement.addEventListener('click', function() {
 		that.object[that.propertyName].call(that.object);
 	}, false);
 	this.domElement.style.cursor = "pointer";
 	this.propertyNameElement.style.cursor = "pointer";
 };
-FunctionController.prototype = new Controller();
-FunctionController.prototype.constructor = FunctionController;
+GUI.extendController(GUI.FunctionController);
