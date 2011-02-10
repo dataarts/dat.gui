9520275b4d97bdd3a062660c368637ff222d6e18
Szymon Nowak <szimek@gmail.com>
2011-02-10 21:50:57 +0100

    Removed trailing whitespace. Converted tabs to 4 spaces.

--- controllers/controller.boolean.js
+++ controllers/controller.boolean.js
@@ -1,38 +1,38 @@
 GUI.BooleanController = function() {
 
-	this.type = "boolean";
-	GUI.Controller.apply(this, arguments);
+    this.type = "boolean";
+    GUI.Controller.apply(this, arguments);
 
-	var _this = this;
+    var _this = this;
     var input = document.createElement('input');
     input.setAttribute('type', 'checkbox');
-    
+
     this.domElement.addEventListener('click', function(e) {
-    	input.checked = !input.checked;
-    	e.preventDefault();
-    	_this.setValue(input.checked);
+        input.checked = !input.checked;
+        e.preventDefault();
+        _this.setValue(input.checked);
     }, false);
-    
+
     input.addEventListener('mouseup', function(e) {
-    	input.checked = !input.checked; // counteracts default.
+        input.checked = !input.checked; // counteracts default.
     }, false);
-    
+
     this.domElement.style.cursor = "pointer";
     this.propertyNameElement.style.cursor = "pointer";
     this.domElement.appendChild(input);
-    
+
     this.updateDisplay = function() {
-    	input.checked = _this.getValue();
+        input.checked = _this.getValue();
     };
-    
-    
+
+
     this.setValue = function(val) {
-		if (typeof val != "boolean") {
-			try { 
-				val = eval(val);
-			} catch (e) {}
-		}
-		return GUI.Controller.prototype.setValue.call(this, val);
+        if (typeof val != "boolean") {
+            try {
+                val = eval(val);
+            } catch (e) {}
+        }
+        return GUI.Controller.prototype.setValue.call(this, val);
     }
 
 };
--- controllers/slider.js
+++ controllers/slider.js
@@ -1,69 +1,69 @@
 GUI.Slider = function(numberController, min, max, step, initValue) {
-	
-	var min = min;
-	var max = max;
-	var step = step;
-	
-	var clicked = false;
-	var _this = this;
-	
-	var x, px;
-	
-	this.domElement = document.createElement('div');
-	this.domElement.setAttribute('class', 'guidat-slider-bg');
-	
-	this.fg = document.createElement('div');
-	this.fg.setAttribute('class', 'guidat-slider-fg');
-	
-	this.domElement.appendChild(this.fg);
-	
-	var findPos = function(obj) {
-		var curleft = curtop = 0;
-		if (obj.offsetParent) {
-			do {
-				curleft += obj.offsetLeft;
-				curtop += obj.offsetTop;
-			} while (obj = obj.offsetParent);
-			return [curleft,curtop];
-		}
-	}
-	
-	this.__defineSetter__('value', function(e) {
-		var pct = GUI.map(e, min, max, 0, 100);
-		this.fg.style.width = pct+"%";
-	});
 
-	var onDrag = function(e) {
-		if (!clicked) return;
-		var pos = findPos(_this.domElement);
-		var val = GUI.map(e.pageX, pos[0], pos[0] + _this.domElement.offsetWidth, min, max);
-		val = Math.round(val/step)*step;
-		numberController.setValue(val);
-	}
-	
-	this.domElement.addEventListener('mousedown', function(e) {
-		clicked = true;
-		x = px = e.pageX;
-		_this.domElement.setAttribute('class', 'guidat-slider-bg active');
-		_this.fg.setAttribute('class', 'guidat-slider-fg active');
-		onDrag(e);
-		document.addEventListener('mouseup', mouseup, false);
-	}, false);
-	
-	
-	var mouseup = function(e) { 
-		_this.domElement.setAttribute('class', 'guidat-slider-bg');
-		_this.fg.setAttribute('class', 'guidat-slider-fg');
-		clicked = false;			
-		if (numberController.finishChangeFunction != null) {
-			numberController.finishChangeFunction.call(this, numberController.getValue());
-		}
-		document.removeEventListener('mouseup', mouseup, false);
-	};
+    var min = min;
+    var max = max;
+    var step = step;
 
-	
-	document.addEventListener('mousemove', onDrag, false);
-	
-	this.value = initValue;	
-		
+    var clicked = false;
+    var _this = this;
+
+    var x, px;
+
+    this.domElement = document.createElement('div');
+    this.domElement.setAttribute('class', 'guidat-slider-bg');
+
+    this.fg = document.createElement('div');
+    this.fg.setAttribute('class', 'guidat-slider-fg');
+
+    this.domElement.appendChild(this.fg);
+
+    var findPos = function(obj) {
+        var curleft = curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curleft += obj.offsetLeft;
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return [curleft,curtop];
+        }
+    }
+
+    this.__defineSetter__('value', function(e) {
+        var pct = GUI.map(e, min, max, 0, 100);
+        this.fg.style.width = pct+"%";
+    });
+
+    var onDrag = function(e) {
+        if (!clicked) return;
+        var pos = findPos(_this.domElement);
+        var val = GUI.map(e.pageX, pos[0], pos[0] + _this.domElement.offsetWidth, min, max);
+        val = Math.round(val/step)*step;
+        numberController.setValue(val);
+    }
+
+    this.domElement.addEventListener('mousedown', function(e) {
+        clicked = true;
+        x = px = e.pageX;
+        _this.domElement.setAttribute('class', 'guidat-slider-bg active');
+        _this.fg.setAttribute('class', 'guidat-slider-fg active');
+        onDrag(e);
+        document.addEventListener('mouseup', mouseup, false);
+    }, false);
+
+
+    var mouseup = function(e) {
+        _this.domElement.setAttribute('class', 'guidat-slider-bg');
+        _this.fg.setAttribute('class', 'guidat-slider-fg');
+        clicked = false;
+        if (numberController.finishChangeFunction != null) {
+            numberController.finishChangeFunction.call(this, numberController.getValue());
+        }
+        document.removeEventListener('mouseup', mouseup, false);
+    };
+
+
+    document.addEventListener('mousemove', onDrag, false);
+
+    this.value = initValue;
+
 }
