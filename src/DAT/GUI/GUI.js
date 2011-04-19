356a7caf5abde2b64126f53a4e489d200c0238c2
George Michael Brower <georgebrower@google.com>
2011-04-18 17:46:18 -0700

    finished recreating work that git so mercilessly overwrote.

--- src/DAT/GUI/GUI.js
+++ src/DAT/GUI/GUI.js
@@ -275,6 +275,14 @@ DAT.GUI = function(parameters) {
 
   this.add = function() {
 
+    if (arguments.length == 1) {
+      var toReturn = [];
+      for (var i in arguments[0]) {
+        toReturn.push(_this.add(arguments[0], i));
+      }
+      return toReturn;
+    }
+
     var object = arguments[0];
     var propertyName = arguments[1];
 
@@ -693,5 +701,3 @@ DAT.GUI.removeClass = function(domElement, className) {
 if (DAT.GUI.getVarFromURL('saveString') != null) {
   DAT.GUI.load(DAT.GUI.getVarFromURL('saveString'));
 }
-
-window['DAT'] = DAT;
