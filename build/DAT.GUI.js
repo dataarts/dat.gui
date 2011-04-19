b987b4875807123e1e9e51a1739ecb5a4d8a63a2
Doug Fritz <dougfritz@gmail.com>
2011-04-19 12:17:17 -0700

    added apache 2.0 license to build script

--- build/DAT.GUI.js
+++ build/DAT.GUI.js
@@ -1,3 +1,15 @@
+/**
+ * dat.gui Javascript Controller Library
+ * http://dataarts.github.com/dat.gui
+ *
+ * Copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 var DAT = DAT || {};
 
 DAT.GUI = function(parameters) {
