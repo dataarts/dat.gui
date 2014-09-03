d3cec695961d982a7d8b2ba8a54270fcae14fc11
Doug Fritz <dougfritz@google.com>
2014-09-03 16:14:14 -0700

    update gulpfile and add a linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,91 @@
-var gulp    = require( 'gulp' ),
-    stylus  = require( 'gulp-stylus' ),
-    plates  = require( 'gulp-plates' ),
-    rename  = require( 'gulp-rename' ),
-    vulcan  = require( 'gulp-vulcanize' ),
-    insert  = require( 'gulp-insert' ),
-    replace = require( 'gulp-replace' ),
-    clean   = require( 'gulp-clean' ),
-    nib     = require( 'nib' ),
-    fs      = require( 'fs' ),
-    marked  = require( 'marked' ),
-    karma   = require( 'karma' );
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  nib = require('nib'),
+  fs = require('fs'),
+  marked = require('marked'),
+  karma = require('karma');
 
-gulp.task( 'default', [ 'docs', 'build' ] );
+gulp.task('default', ['docs', 'build']);
 
-gulp.task( 'watch', [ 'default' ], function() {
-    
-    karma.server.start( {
-        frameworks: [ 'jasmine' ],
-        files: [
-          'build/gui.js',
-          'tests/*.js'
-        ]
-    } );
+gulp.task('watch', ['default'], function() {
 
-    gulp.watch( [ 'elements/**/*.styl', 'elements/**/*.html', 'elements/**/*.js', 'gui.html' ], [ 'build' ] );
-    gulp.watch( [ 'README.md', 'docs/*' ], [ 'docs' ] );
+  karma.server.start({
+    frameworks: ['jasmine'],
+    files: [
+      'build/gui.js',
+      'tests/*.js'
+    ]
+  });
 
-} );
+  gulp.watch(['elements/**/*.styl', 'elements/**/*.html',
+              'elements/**/*.js', 'gui.html'], ['build']);
+  gulp.watch(['README.md', 'docs/*'], ['docs']);
 
-gulp.task( 'build', [ 'vulcanize' ], function() {
+});
 
-    return gulp.src( 'build/gui.html' )
-               .pipe( replace( /\\/g, "\\\\" ) )
-               .pipe( replace( /'/g, "\\'" ) )
-               .pipe( replace( /^(.*)$/gm, "'$1'," ) )
-               .pipe( insert.wrap( 'document.write([', '].join("\\n"))' ) )
-               .pipe( rename( 'gui.js' ) )
-               .pipe( gulp.dest( 'build' ) );
+gulp.task('build', ['vulcanize'], function() {
 
-} );
+  return gulp.src('build/gui.html')
+         .pipe($.replace(/\\/g, '\\\\'))
+         .pipe($.replace(/'/g, "\\'"))
+         .pipe($.replace(/^(.*)$/gm, "'$1',"))
+         .pipe($.insert.wrap('document.write([', '].join("\\n"))'))
+         .pipe(rename('gui.js'))
+         .pipe(gulp.dest('build'));
 
-gulp.task( 'vulcanize', [ 'css' ], function() {
+});
 
-    return gulp.src( 'gui.html' )
-               .pipe( vulcan( { 
-                   dest: 'build', 
-                   inline: true,
-                   strip: true 
-                } ) );
+gulp.task('vulcanize', ['css'], function() {
 
-} );
+  return gulp.src('gui.html')
+         .pipe($.vulcanize({
+           dest: 'build',
+           inline: true,
+           strip: true
+        }));
 
-gulp.task( 'css', function() {
+});
 
-    return css( 'elements/**/*.styl', 'elements' );
+gulp.task('lint', function() {
+  return gulp.src('elements/**/*.js')
+    //.pipe($.reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'));
+    //.pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
 
-} );
+gulp.task('css', function() {
 
-gulp.task( 'docs', function() {
-        
-    css( 'docs/*.styl', 'docs' );
+  return css('elements/**/*.styl', 'elements');
 
-    var content = {
-        readme: marked( fs.readFileSync( 'README.md', 'utf8' ) )
-    };
+});
 
-    return gulp.src( 'docs/template.html' )
-        .pipe( plates( content ) )
-        .pipe( rename( 'index.html' ) )
-        .pipe( gulp.dest( './' ) );
+gulp.task('docs', function() {
 
-} );
+  css('docs/*.styl', 'docs');
 
-gulp.task( 'clean', function() {
+  var content = {
+    readme: marked(fs.readFileSync('README.md', 'utf8'))
+  };
 
-    return gulp.src( [ 'build/*', '**/*.css' ] )
-               .pipe( clean() );
+  return gulp.src('docs/template.html')
+    .pipe($.plates(content))
+    .pipe($.rename('index.html'))
+    .pipe(gulp.dest('./'));
 
-} );
+});
 
-function css( src, dest ) {
+gulp.task('clean', function() {
 
-    return gulp.src( src )
-               .pipe( stylus( { use: [ nib() ] } ) )
-               .pipe( gulp.dest( dest ) );
+  return gulp.src(['build/*', '**/*.css'])
+         .pipe($.clean());
+
+});
+
+function css(src, dest) {
+
+  return gulp.src(src)
+         .pipe($.stylus({ use: [nib()] }))
+         .pipe(gulp.dest(dest));
 
 }
