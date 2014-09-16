d3a74283d15fdb5dfc76f0f4456d3de145dacfa3
George Corney <haxiomic@gmail.com>
2014-09-16 11:24:10 +0100

    Added requestAnimationFrame
    To fix the warning " 'webkitRequestAnimationFrame' is vendor-specific. Please use the standard 'requestAnimationFrame' instead. " in more recent versions of Chrome

--- src/dat/utils/requestAnimationFrame.js
+++ src/dat/utils/requestAnimationFrame.js
@@ -19,7 +19,8 @@ define([
    * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
    */
 
-  return window.webkitRequestAnimationFrame ||
+  return window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
       window.mozRequestAnimationFrame ||
       window.oRequestAnimationFrame ||
       window.msRequestAnimationFrame ||
