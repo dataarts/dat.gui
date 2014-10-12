86f43c0be5db08c9a6d7339aa8287620306fb0b5
Ulysses Popple <ulysses.popple@gmail.com>
2014-10-12 15:32:48 -0400

    Fix remove method to use splice instead of slice

--- src/dat/gui/GUI.js
+++ src/dat/gui/GUI.js
@@ -544,7 +544,7 @@ define([
 
           // TODO listening?
           this.__ul.removeChild(controller.__li);
-          this.__controllers.slice(this.__controllers.indexOf(controller), 1);
+          this.__controllers.splice(this.__controllers.indexOf(controller), 1);
           var _this = this;
           common.defer(function() {
             _this.onResize();
