aba22511c3335e04ad8bdc3ea85b06a3cb6d59f1
Fabian Irsara <info@fabianirsara.com>
2015-12-01 22:37:07 +0100

    compile

--- build/dat.gui.js
+++ build/dat.gui.js
@@ -1920,10 +1920,14 @@ dat.GUI = dat.gui.GUI = (function (css, saveDialogueContents, styleSheet, contro
 
     }
 
-    dom.bind(window, 'resize', function() { _this.onResize() });
-    dom.bind(this.__ul, 'webkitTransitionEnd', function() { _this.onResize(); });
-    dom.bind(this.__ul, 'transitionend', function() { _this.onResize() });
-    dom.bind(this.__ul, 'oTransitionEnd', function() { _this.onResize() });
+    var _resizeHandler = function () {
+      _this.onResize();
+    };
+
+    dom.bind(window, 'resize', _resizeHandler);
+    dom.bind(this.__ul, 'webkitTransitionEnd', _resizeHandler);
+    dom.bind(this.__ul, 'transitionend', _resizeHandler);
+    dom.bind(this.__ul, 'oTransitionEnd', _resizeHandler);
     this.onResize();
 
 
@@ -1977,14 +1981,14 @@ dat.GUI = dat.gui.GUI = (function (css, saveDialogueContents, styleSheet, contro
   GUI.TEXT_CLOSED = 'Close Controls';
   GUI.TEXT_OPEN = 'Open Controls';
 
-  dom.bind(window, 'keydown', function(e) {
-
+  var _keydownHandler = function(e) {
     if (document.activeElement.type !== 'text' &&
         (e.which === HIDE_KEY_CODE || e.keyCode == HIDE_KEY_CODE)) {
       GUI.toggleHide();
     }
+  };
 
-  }, false);
+  dom.bind(window, 'keydown', _keydownHandler, false);
 
   common.extend(
 
@@ -2052,6 +2056,10 @@ dat.GUI = dat.gui.GUI = (function (css, saveDialogueContents, styleSheet, contro
           if (this.autoPlace) {
             auto_place_container.removeChild(this.domElement);
           }
+
+          dom.unbind(window, 'keydown', _keydownHandler, false);
+          dom.unbind(window, 'unload', saveToLocalStorage);
+          dom.unbind(window, 'resize', _resizeHandler);
 
         },
 
