922de644599ad63149bd9b686a252e5e6164d83a
Jeff Nusz <jeff@custom-logic.com>
2016-08-17 16:28:11 -0700

    fix NumberBox not showing step() changes to slider

--- src/dat/gui/GUI.js
+++ src/dat/gui/GUI.js
@@ -819,7 +819,7 @@ function augmentController(gui, li, controller) {
     const box = new NumberControllerBox(controller.object, controller.property,
       {min: controller.__min, max: controller.__max, step: controller.__step});
 
-    common.each(['updateDisplay', 'onChange', 'onFinishChange'], function(method) {
+    common.each(['updateDisplay', 'onChange', 'onFinishChange', 'step'], function(method) {
       const pc = controller[method];
       const pb = box[method];
       controller[method] = box[method] = function() {
