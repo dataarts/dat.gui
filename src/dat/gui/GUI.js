3aaa59d225236ab562e3a630edd60ef105f18ba0
Shun Git <kwokshungit@gmail.com>
2017-10-09 09:47:52 +0800

    add removeFolder. From dataarts/dat.gui#143

--- src/dat/gui/GUI.js
+++ src/dat/gui/GUI.js
@@ -583,6 +583,20 @@ common.extend(
       return gui;
     },
 
+    /**
+     * @param name
+     */
+    removeFolder: function(name) {
+      var folder = this.__folders[name];
+      if (!folder) {
+        return;
+      }
+      folder.close();
+      this.__ul.removeChild(folder.domElement.parentNode);
+      delete this.__folders[name];
+      this.onResize();
+    },
+
     open: function() {
       this.closed = false;
     },
