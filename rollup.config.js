1cf95b490f5c8c03d2614079759c6ee3b17731a3
Don McCurdy <don.r.mccurdy@gmail.com>
2018-01-20 00:01:41 -0800

    Use Rollup for UMD and ES6 builds.

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+/**
+ * dat-gui JavaScript Controller Library
+ * http://code.google.com/p/dat-gui
+ *
+ * Copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import fs from 'fs';
+import path from 'path';
+import resolve from 'rollup-plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
+import babel from 'rollup-plugin-babel';
+import sass from 'rollup-plugin-sass';
+
+const banner = fs.readFileSync(path.join(__dirname, 'licenseBanner.txt'));
+
+export default {
+  input: 'src/dat/index.js',
+  output: [{
+    file: './build/dat.gui.js',
+    format: 'umd',
+    name: 'dat',
+    banner: banner
+  }, {
+    file: './build/dat.gui.module.js',
+    format: 'es',
+    banner: banner
+  }],
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    resolve(),
+    sass({options: {outputStyle: 'compressed'}}),
+    babel({
+      plugins: ['external-helpers'],
+      exclude: 'node_modules/**'
+    }),
+    cleanup()
+  ]
+};
