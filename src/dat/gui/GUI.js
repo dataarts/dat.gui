5730da37c4076f1232c6f613becf291e1a5bf913
AARON WELLES <aaron.welles@gmail.com>
2019-06-19 02:21:21 -0400

    .add( controller )

--- src/dat/gui/GUI.js
+++ src/dat/gui/GUI.js
@@ -168,7 +168,7 @@ const GUI = function(pars) {
   if (params.autoPlace && common.isUndefined(params.scrollable)) {
     params.scrollable = true;
   }
-  //    params.scrollable = common.isUndefined(params.parent) && params.scrollable === true;
+//    params.scrollable = common.isUndefined(params.parent) && params.scrollable === true;
 
   // Not part of params because I don't want people passing this in via
   // constructor. Should be a 'remembered' value.
