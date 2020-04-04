b0c59d6a710d89a9361c68d96105c588d7b1e215
Don McCurdy <donmccurdy@google.com>
2020-04-03 22:29:37 -0700

    0.7.7

--- build/dat.gui.js
+++ build/dat.gui.js
@@ -186,7 +186,7 @@ var Common = {
     return obj === false || obj === true;
   },
   isFunction: function isFunction(obj) {
-    return Object.prototype.toString.call(obj) === '[object Function]';
+    return obj instanceof Function;
   }
 };
 
@@ -684,8 +684,9 @@ Object.defineProperty(Color.prototype, 'a', {
 });
 Object.defineProperty(Color.prototype, 'hex', {
   get: function get$$1() {
-    if (!this.__state.space !== 'HEX') {
+    if (this.__state.space !== 'HEX') {
       this.__state.hex = ColorMath.rgb_to_hex(this.r, this.g, this.b);
+      this.__state.space = 'HEX';
     }
     return this.__state.hex;
   },
